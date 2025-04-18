78d518f4a0e0be94d8ba39eca2a0052792951348
Datchany <rdatchany530@gmail.com>
2025-04-18 14:28:34 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from "./components/Home";
+import Cart from "./components/Cart";
+import { CartProvider } from "react-use-cart";
+
+
+function App() {
+  return (
+    <>
+      <CartProvider>
+        <Home />
+        <Cart />
+       
+      </CartProvider>
+    </>
+  );
+}
+
+export default App;
